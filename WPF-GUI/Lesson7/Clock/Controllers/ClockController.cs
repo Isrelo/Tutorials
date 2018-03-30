af7a347c33af947b8c7381a389e36051bc93ec88
Isrelo <gameobjectcreator@gmail.com>
2018-03-29 21:09:25 -0700

    Lesson 6 covered the start of the Model View Controller (MVC) pattern for a digital Clock. Lesson 7 covered corrections to the clock program to make it update correctly and covered how to add multiple views to a single controller.

---
+++ WPF-GUI/Lesson7/Clock/Controllers/ClockController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Clock.Models;
+using Clock.Views;
+
+namespace Clock.Controllers
+{
+    public class ClockController
+    {
+        TimeModel TimeData_m;
+        List<IView> ClockViews_m;
+
+        public ClockController()
+        {
+            ClockViews_m = new List<IView>();
+        }
+
+        public void AddClockView(IView clock_view_p)
+        {
+            ClockViews_m.Add(clock_view_p);
+        }
+
+        public void UpdateTime()
+        {
+            DateTime current_time = DateTime.Now;
+
+            TimeData_m = new TimeModel();
+            TimeData_m.Hours = current_time.Hour;
+            TimeData_m.Minutes = current_time.Minute;
+            TimeData_m.Seconds = current_time.Second;
+            TimeData_m.AmOrPm = current_time.ToString("tt");
+
+            foreach(IView temp_clock_view in ClockViews_m)
+            {
+                temp_clock_view.Display(TimeData_m);
+            }
+        }
+    }
+}
---
+++ WPF-GUI/Lesson7/Clock/Views/DigitialClockView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Clock.Models;
+
+namespace Clock.Views
+{
+    /// <summary>
+    /// Interaction logic for DigitialClockView.xaml
+    /// </summary>
+    public partial class DigitialClockView : UserControl, IView
+    {
+        public DigitialClockView()
+        {
+            InitializeComponent();
+        }
+
+        public void Display(TimeModel time_modle_p)
+        {
+            TimeDisplay_txtb.Text = time_modle_p.Hours.ToString("D2") + ":" + time_modle_p.Minutes.ToString("D2") + ":" + time_modle_p.Seconds.ToString("D2") + time_modle_p.AmOrPm;
+        }
+    }
+}
