19f2c97519dc0bb13c2283039257eb6cc74775c8
Isrelo <gameobjectcreator@gmail.com>
2018-05-16 23:31:15 -0700

    Lesson 11 covers starting to lay the groundwork for creating the applications toolbar.

---
+++ WPF-GUI/Lesson11/TransactionRegister/ViewModels/TollbarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace TransactionRegister
+{
+    public class TollbarViewModel
+    {
+        public Brush Background_Color { get; set; }
+        public Brush Font_Color { get; set; }
+        public Button Navigation_btn { get; set; }
+        public Label Title_lbl { get; set; }
+        public List<Button> ItemModifers { get; set; }
+
+        public TollbarViewModel()
+        {
+            Background_Color = Brushes.White;
+            Font_Color = Brushes.White;
+            Navigation_btn = new Button();
+            Title_lbl = new Label();
+            ItemModifers = new List<Button>();
+        }
+    }
+}
---
+++ WPF-GUI/Lesson11/TransactionRegister/Views/Toolbar.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//using TransactionRegister.ViewModels;
+
+namespace TransactionRegister
+{
+    /// <summary>
+    /// Interaction logic for Toolbar.xaml
+    /// </summary>
+    public partial class Toolbar : UserControl
+    {
+        public Toolbar()
+        {
+            InitializeComponent();
+
+            TollbarViewModel tool_bar_view_model = new TollbarViewModel();
+            tool_bar_view_model.Background_Color = Brushes.Blue;
+            tool_bar_view_model.Font_Color = Brushes.White;
+
+            this.DataContext = tool_bar_view_model;
+        }
+
+    }
+}
