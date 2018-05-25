d3b41faa96967b9131ef12f0ee879fe2205b0e03
Isrelo <gameobjectcreator@gmail.com>
2018-05-24 22:35:27 -0700

    Lesson 13 covers how to dynamically assign buttons to a StackPanel.

---
+++ WPF-GUI/Lesson13/TransactionRegister/ViewModels/TollbarViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace TransactionRegister
+{
+    public class TollbarViewModel
+    {
+        public Brush Background_Color { get; set; }
+        public Brush Font_Color { get; set; }
+        public Button Naigation_btn { get; set; }
+        public Label Title_lbl { get; set; }
+        public List<Button> ItemModifers { get; set; }
+
+        public TollbarViewModel()
+        {
+            Background_Color = Brushes.White;
+            Font_Color = Brushes.White;
+            Naigation_btn = new Button();
+            Title_lbl = new Label();
+            ItemModifers = new List<Button>();
+        }
+
+        public Button BuildImageButton(String image_file_p, RoutedEventHandler button_click_p)
+        {
+            // Create a image to dsiplay on the button.
+            Image button_image = new Image();
+            button_image.Width = 24;
+            button_image.Height = 24;
+            button_image.Source = new BitmapImage(new Uri(image_file_p, UriKind.Relative));
+
+            // Create actual image button.
+            Button image_button = new Button();
+            image_button.Width = 56;
+            image_button.Height = 56;
+            image_button.Background = Brushes.Transparent;
+            image_button.BorderBrush = Brushes.Transparent;
+            image_button.Click += button_click_p;
+            image_button.Content = button_image;
+
+            return image_button;
+        }
+    }
+}
---
+++ WPF-GUI/Lesson13/TransactionRegister/Views/Toolbar.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//using TransactionRegister.ViewModels;
+
+namespace TransactionRegister
+{
+    /// <summary>
+    /// Interaction logic for Toolbar.xaml
+    /// </summary>
+    public partial class Toolbar : UserControl
+    {
+        public Toolbar()
+        {
+            InitializeComponent();
+
+            TollbarViewModel tool_bar_view_model = new TollbarViewModel();
+            tool_bar_view_model.Background_Color = Brushes.Blue;
+            tool_bar_view_model.Font_Color = Brushes.White;
+
+            // Add Item Modifer Buttons
+            tool_bar_view_model.ItemModifers.Add(tool_bar_view_model.BuildImageButton("../images/DeleteWhite.png", DeleteBtn_Click));
+            tool_bar_view_model.ItemModifers.Add(tool_bar_view_model.BuildImageButton("../images/EditWhite.png", EditBtn_Click));
+            
+            // Display the navigation button in our parrent container.
+            NavigationSp.Children.Add(tool_bar_view_model.BuildImageButton("../images/BackArrowWhite.png", NavigationBtn_Click));
+
+            // Display Item Modifer Buttons
+            foreach (Button temp_button in tool_bar_view_model.ItemModifers)
+            {
+                ItemModifersSp.Children.Add(temp_button);
+            }
+
+
+            this.DataContext = tool_bar_view_model;
+        }
+
+        private void NavigationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Clicked navigation button!");
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Clicked edit button!");
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Clicked delete button!");
+        }
+    }
+}
