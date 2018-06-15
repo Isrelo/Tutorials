197f25b537a665e78d428ff2c9dec5a11ece11c8
Isrelo <gameobjectcreator@gmail.com>
2018-06-14 21:23:09 -0700

    Lesson 15 - Accidentally deleted unit test code so here it is.

---
+++ WPF-GUI/Lesson15/TransactionRegisterTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TransactionRegisterTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TransactionRegisterTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c38f74f0-aca9-4014-a52d-e9d9f11b05a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
