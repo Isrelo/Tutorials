d937c54eec794f817a432f5158c422c36e018e83
Isrelo <gameobjectcreator@gmail.com>
2018-08-09 21:29:32 -0700

    Lesson 20 covers adding the transactions custom control.

---
+++ WPF-GUI/Lesson20/States/EditTransactionState.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TransactionRegister.Contexts;
+using TransactionRegister.Controllers;
+
+namespace TransactionRegister.States
+{
+    public class EditTransactionState : IState
+    {
+        public ToolbarController ToolbarController_m { get; set; }
+        public WorkspaceController WorkspaceController_m { get; set; }
+        public ApplicationContext ApplicationContext_m { get; set; }
+
+        public void SaveTransaction(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cancel(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Not Used Staes
+
+        public void AddTransaction(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTransaction(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditTransaction(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SelectTransaction(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Starting(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnselectTransaction(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SwitchToState(ApplicationContext app_context_p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF-GUI/Lesson20/ViewModels/TollbarViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using TransactionRegister.Models;
+
+namespace TransactionRegister.ViewModels
+{
+    public class ToolbarViewModel : BaseModel
+    {
+        public Brush Background_Color { get; set; }
+        public Brush Font_Color { get; set; }
+        public Button Naigation_btn { get; set; }
+        public String Title { get; set; }
+        public List<Button> ItemModifers { get; set; }
+
+        public ToolbarViewModel()
+        {
+            // Initialize Default Colors.
+            Background_Color = Brushes.White;
+            Font_Color = Brushes.White;
+
+            // Initialize Navigation Button
+            Naigation_btn = new Button();
+            Naigation_btn.Background = Brushes.Transparent;
+            Naigation_btn.BorderBrush = Brushes.Transparent;
+            Naigation_btn.Visibility = Visibility.Hidden;
+
+            // Initialize Title
+            Title = "";
+
+            // Initialize Item Modifiers List.
+            ItemModifers = new List<Button>();
+        }
+    }
+}
