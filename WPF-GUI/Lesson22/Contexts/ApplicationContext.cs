e5b994423733163269fc0d174a8524b670a25477
Isrelo <gpoliece@gmail.com>
2018-08-31 20:59:38 -0700

    Lesson 22 covers how to format and display ListBox items.

---
+++ WPF-GUI/Lesson22/Contexts/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TransactionRegister.States;
+
+namespace TransactionRegister.Contexts
+{
+    public class ApplicationContext
+    {
+        public IState CurrentState_m { get; set; }
+
+        public ApplicationContext(IState state_to_switch_to_p)
+        {
+            CurrentState_m = state_to_switch_to_p;
+        }
+    }
+}
---
+++ WPF-GUI/Lesson22/Models/Transaction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TransactionRegister.Models
+{
+    public class Transaction : BaseModel, INotifyPropertyChanged
+    {
+        private String description_m;
+        public String Description
+        {
+            get { return description_m; }
+            set
+            {
+                description_m = value;
+
+                // Required Changed Event
+                NotifyPropertyChanged();
+            }
+        }
+        private double balance_m;
+        public double Balance
+        {
+            get { return balance_m; }
+            set
+            {
+                balance_m = value;
+
+                // Required Changed Event
+                NotifyPropertyChanged();
+            }
+        }
+        private bool debit_or_credit_m;
+        public bool DebitOrCredit
+        {
+            get { return debit_or_credit_m; }
+            set
+            {
+                debit_or_credit_m = value;
+
+                // Required Changed Event
+                NotifyPropertyChanged();
+            }
+        }
+        private double amount_m;
+        public double Amount
+        {
+            get { return amount_m; }
+            set
+            {
+                amount_m = value;
+
+                // Required Changed Event
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property. 
+        // The CallerMemberName attribute that is applied to the optional propertyName 
+        // parameter causes the property name of the caller to be substituted as an argument. 
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
